5255f7d3aeebf850cef64bab07aca2e55ad84086
Kevin Angulo <kevin.angulo100@gmail.com>
2021-04-18 20:46:14 -0400

    imagenes por columnas

--- lib/FoodCounter.dart
+++ lib/FoodCounter.dart
@@ -1,30 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+//import 'package:intl/intl.dart';
 
 class FoodCounter extends StatelessWidget {
   const FoodCounter({Key key}) : super(key: key);
 
-  DateTime _currentDate = new DateTime.now();
+  //DateTime _currentDate = new DateTime.now();
 
-  Future<Null> selectdate(BuildContext context) async {
+  /*Future<Null> selectdate(BuildContext context) async {
     final DateTime _seldate = await showDatePicker(
         context: context,
         initialDate: _currentDate,
         firstDate: DateTime(1990),
         lastDate: DateTime(2021));
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
-    String _formatdate = new DateFormat.yMMMd().format(_currentDate);
+     //String _formatdate = new DateFormat.yMMMd().format(_currentDate);
     final _TabPages = <Widget>[
-      const Center(child: Icon(Icons.home)),
-      const Center(child: Icon(Icons.analytics)),
-      const Center(child: Icon(Icons.calendar_today_rounded)),
+      GridView.count(
+        crossAxisCount: 3,
+        children: <Widget>[
+                    Card(
+                      
+                     elevation: 5,
+                       child: Container(
+                        //Alignment.centerLeft es Alignment(-1.0, 0.0),
+                        child: Image.asset('assets/panes.png',width:200)
+                     )
+                   ),
+                     Card(
+                     elevation: 5,
+                       child: Container(
+                        child: Image.asset('assets/acompañantes.jpg',width:200)
+               )
+            ),
+             Card(
+                     elevation: 5,
+                       child: Container(
+                        child: Image.asset('assets/bebidaCal.jfif',width:200)
+               )
+            )
+        ]
+      ),
+       Center(child: Icon(Icons.analytics)),
+       Center(child: Icon(Icons.calendar_today_rounded)),
       const Center(child: Icon(Icons.star)),
-      const Center(child: text('Date: $_currentDate')),
+      //const Center(child: Text("Date: $_currentDate")),
     ];
-    final _KTabs = <Tab>[
+      final _KTabs = <Tab>[
       const Tab(icon: Icon(Icons.home), text: 'Inicio'),
       const Tab(icon: Icon(Icons.analytics), text: 'Estadísticas'),
       const Tab(icon: Icon(Icons.calendar_today_rounded), text: 'Calendario'),
