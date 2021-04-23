3809441baa6bf92d52b66f098081c2277c4e23b2
Anghelo Penia <luisanghelopena@gmail.com>
2021-04-23 18:35:20 -0400

    Merge FoodImplementation

--- lib/FoodSelector.dart
+++ lib/FoodSelector.dart
@@ -88,7 +88,7 @@ class _FoodSelectorState extends State<FoodSelector> {
                               },
                             )
                           ],
-                        )
+                        ) 
                       ]))))
         ]));
   }
