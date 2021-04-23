64efad5234d54723eea05e33509c943881373510
Formvoid-83 <guillermomolinaggg@gmail.com>
2021-04-23 01:08:30 -0400

    Flexibilidad de las comidas en imagenes, titulos e items

--- lib/FoodCounter.dart
+++ lib/FoodCounter.dart
@@ -1,19 +1,39 @@
+import 'dart:html';
 import 'package:flutter/material.dart';
-class FoodCounter extends StatefulWidget {
-  FoodCounter({Key key}) : super(key: key);
+import 'package:a_bit_of_health/providers/FoodProvider.dart';
+import 'package:a_bit_of_health/models/FoodModel.dart';
 
+class FoodCounter extends StatelessWidget {
+  //const FoodCounter({Key key}) : super(key: key);
+
+//The date displayed in the app
   @override
-  _FoodCounterState createState() => _FoodCounterState();
+  Widget build(BuildContext context) {
+    return NextPage();
+  }
 }
 
-class _FoodCounterState extends State<FoodCounter> {
- 
+class NextPage extends StatelessWidget {
+  const NextPage({Key key}) : super(key: key);
+
+  // DateTime _currentDate = new DateTime.now();
   @override
   Widget build(BuildContext context) {
     // String _formatdate = new DateFormat.yMMMd().format(_currentDate);
+
+    List<String> strArr = ['Carnes', 'Guarniciones', 'Vegetales'];
+    List<String> strArr2 = ['pescado', 'lomo', 'fideos', 'arroz', 'lechuga'];
+    List<List<String>> supremeList = [
+      ['pescado', 'lomo'],
+      ['fideos', 'arroz'],
+      ['lechuga']
+    ];
+
     Size size = MediaQuery.of(context).size;
 
-    return Container(
+    final _TabPages = <Widget>[
+      // CENTER OF THE HOME PAGE -----------------------------------------------------------
+      Container(
         padding: EdgeInsets.all(30),
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           Text(
@@ -23,90 +43,58 @@ class _FoodCounterState extends State<FoodCounter> {
           Expanded(
             child: Container(
               //alignment: Alignment.center,
-              child: ListView(
+              child: ListView.builder(
+                // LISTVIEW OF IMAGES ------------------------------------
                 shrinkWrap: true,
                 scrollDirection: Axis.horizontal,
-                children: <Widget>[
-                  Container(
-                      height: 100,
-                      width: 100,
-                      //alignment: Alignment.center,
-                      child: Image.asset(
-                        'assets/calientes.png',
-                        width: 100,
-                        height: 100,
-                      )),
-                  Container(
-                      height: 100,
-                      width: 100,
-                      //alignment: Alignment.center,
-                      child: Image.asset('assets/frios.png',
-                          width: 100, height: 100)),
-                  Container(
-                      height: 100,
-                      width: 100,
-                      //alignment: Alignment.center,
-                      child: Image.asset('assets/masas.png',
-                          width: 100, height: 100)),
-                  Container(
-                      height: 100,
-                      width: 100,
-                      //alignment: Alignment.center,
-                      child: Image.asset('assets/frutas.png',
-                          width: 100, height: 100)),
-                ],
-              ),
+                itemCount: strArr.length,
+                itemBuilder: (context, i) {
+                  var arr = [
+                    'acompanantes',
+                    'aderezos',
+                    'bebidas',
+                    'caldos',
+                    'calientes',
+                    'carnes',
+                    'dulces',
+                    'frios',
+                    'frutas',
+                    'frutos secos',
+                    'guarniciones',
+                    'masas',
+                    'panes',
+                    'populares',
+                    'salados',
+                    'vegetales'
+                  ];
+                  //Provider.of<FoodOfferModel>(context)listen = false );
+                  return ImagestoDisplay(imageName: strArr[i].toLowerCase());
+                },
+              ), //-------------------------------------------------------------------
             ),
           ),
           SizedBox(
               height: 60,
-              child: ListView(
+              child: ListView.builder(
+                  // LISTVIEW OF TITLES OF TYPES OF FOODS---------------
                   shrinkWrap: true,
                   scrollDirection: Axis.horizontal,
-                  children: <Widget>[
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.2,
-                      height: MediaQuery.of(context).size.height * 0.4,
-                      margin: EdgeInsets.all(10),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        'Calientes',
-                        style: TextStyle(
-                            fontStyle: FontStyle.italic, fontSize: 15),
-                      ),
-                      alignment: Alignment.bottomCenter,
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.2,
-                      height: MediaQuery.of(context).size.height * 0.4,
-                      margin: EdgeInsets.all(10),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        'Panes',
-                        style: TextStyle(
-                            fontStyle: FontStyle.italic, fontSize: 15),
-                      ),
-                      alignment: Alignment.bottomCenter,
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.2,
-                      height: MediaQuery.of(context).size.height * 0.4,
-                      margin: EdgeInsets.all(10),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        'Acompañantes',
-                        style: TextStyle(
-                            fontStyle: FontStyle.italic, fontSize: 15),
-                      ),
-                      alignment: Alignment.bottomCenter,
-                    ),
-                  ])),
+                  itemCount: strArr.length,
+                  itemBuilder: (context, i) {
+                    return TitleDisplay(titleName: strArr[i]);
+                  })), //-----------------------------------------------------------
           Expanded(
             //child: Container(
-            child: ListView(
+            child: ListView.builder(
+                // LISTVIEW OF COLUMNS WITH FOODS AND SPINNERS-------
                 shrinkWrap: true,
                 scrollDirection: Axis.horizontal,
-                children: <Widget>[
+                itemCount: supremeList.length,
+                itemBuilder: (context, i) {
+                  return ColumnsDisplay(foods: supremeList[i]);
+                }
+                //     ColumnsDisplay(),
+/*
                   Container(
                     width: MediaQuery.of(context).size.width * 0.2,
                     height: MediaQuery.of(context).size.height * 0.4,
@@ -138,9 +126,11 @@ class _FoodCounterState extends State<FoodCounter> {
                     decoration:
                         BoxDecoration(border: Border.all(color: Colors.black)),
                   ),
-                ]),
+                */
+                ), //------------------------------------------------
           ),
           Row(
+            // ROW OF BUTTONS----------------
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisSize: MainAxisSize.max,
@@ -155,9 +145,34 @@ class _FoodCounterState extends State<FoodCounter> {
                 onPressed: () {},
               )
             ],
-          ),
+          ), //-----------------------------
         ]),
-      );
+      ),
+      //------------------------------------------------------------------------------
+      //CENTER OF THE STATISTICS
+      Center(child: Icon(Icons.analytics)),
+      //------------------------------------------------------------------------------
+      //CENTER OF THEE CALENDAR
+      Center(child: Icon(Icons.calendar_today_rounded)),
+      //------------------------------------------------------------------------------
+      //CENTER OF TODAY
+      Center(child: Icon(Icons.star)),
+      //------------------------------------------------------------------------------
+      //const Center(child: Text("Date: $_currentDate")),
+    ];
+
+    final _KTabs = <Tab>[
+      const Tab(icon: Icon(Icons.home), text: 'Inicio'),
+      const Tab(icon: Icon(Icons.analytics), text: 'Estadísticas'),
+      const Tab(icon: Icon(Icons.calendar_today_rounded), text: 'Calendario'),
+      const Tab(icon: Icon(Icons.star), text: 'Hoy'),
+    ];
+    return DefaultTabController(
+      length: _KTabs.length,
+      child: Scaffold(
+        body: TabBarView(children: _TabPages),
+      ),
+    );
   }
 }
 
@@ -265,4 +280,51 @@ class ImagestoDisplay extends StatelessWidget {
   }
 }
 
+class TitleDisplay extends StatelessWidget {
+  String titleName;
+
+  TitleDisplay({this.titleName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.1,
+      height: MediaQuery.of(context).size.height * 0.1,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      child: Text(
+        titleName,
+        style: TextStyle(fontStyle: FontStyle.italic, fontSize: 15),
+      ),
+      alignment: Alignment.center,
+    );
+  }
+}
+
+class ColumnsDisplay extends StatelessWidget {
+  List<String> foods;
+
+  ColumnsDisplay({this.foods});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.14,
+      height: MediaQuery.of(context).size.height * 0.35,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+      alignment: Alignment.topCenter,
+      child: ListView.builder(
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
+          itemCount: foods.length,
+          itemBuilder: (context, i) {
+            return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text(foods[i]), CounterView()]);
+          }),
+    );
+  }
+}
 //-------------------------------------------------------------------------------
