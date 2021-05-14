f4807f227b6dfed1444cc88f4068e154951cd1d6
NicoleBtte <nicole.bustamante.trujillo@gmail.com>
2021-05-13 20:27:55 -0400

    Inicio y mas frases

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: 'FoodSelector',
+      initialRoute: '/',
       routes: getRoutes(),
   
     ));
