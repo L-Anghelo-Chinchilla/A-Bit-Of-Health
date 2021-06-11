47ff9cb2c0eb2419503cb8188983aeecdec0bb15
danielaestefany <estefanymunoz0@gmail.com>
2021-06-10 23:11:05 -0400

    Call Dates

--- lib/History.dart
+++ lib/History.dart
@@ -6,68 +6,73 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 import 'package:syncfusion_localizations/syncfusion_localizations.dart';
-
+import 'package:tuple/tuple.dart';
 
 class History extends StatelessWidget {
-  const History
-  ({Key key}) : super(key: key);
-
-
-
+  const History({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
     String date1;
     String date2;
-     if(Provider.of<UserModel>(context).userID ==null)
+    if (Provider.of<UserModel>(context).userID == null)
       return Login();
     else
-         return Scaffold(
-      appBar:getAppBar(context:context),
-      body: Container(
-        //GlobalWidgetsLocalizations localizations = Localizations.of<GlobalWidgetsLocalizations>(context, GlobalWidgetsLocalizations);
-        decoration: BoxDecoration(
+      return Scaffold(
+        appBar: getAppBar(context: context),
+        body: Container(
+          //GlobalWidgetsLocalizations localizations = Localizations.of<GlobalWidgetsLocalizations>(context, GlobalWidgetsLocalizations);
+          decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage('fondo_historial.jpg'),
-              fit: BoxFit.cover
-            ),
+                image: AssetImage('fondo_historial.jpg'), fit: BoxFit.cover),
           ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-
-          children: [
-            getDirectionsBar(context , 'History'),
-            Expanded(child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  child: Text("Selecciona el intervalo de fechas para ver tu historial", style: TextStyle(fontFamily: 'Mont', fontSize: 28)),
-                ),
-                Container(
-                  color: Colors.white.withOpacity(0.8),
-                  width: MediaQuery.of(context).size.width/2.5,
-                  height: MediaQuery.of(context).size.height/2,
-                 child: SfDateRangePicker(
-                      onSelectionChanged: (DateRangePickerSelectionChangedArgs args) {
-                        date1 = args.value.startDate.toString().split(' ').first;
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              getDirectionsBar(context, 'History'),
+              Expanded(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    child: Text(
+                        "Selecciona el intervalo de fechas para ver tu historial",
+                        style: TextStyle(fontFamily: 'Mont', fontSize: 28)),
+                  ),
+                  Container(
+                    color: Colors.white.withOpacity(0.8),
+                    width: MediaQuery.of(context).size.width / 2.5,
+                    height: MediaQuery.of(context).size.height / 2,
+                    child: SfDateRangePicker(
+                      onSelectionChanged:
+                          (DateRangePickerSelectionChangedArgs args) {
+                        date1 =
+                            args.value.startDate.toString().split(' ').first;
                         date2 = args.value.endDate.toString().split(' ').first;
                         print(date1);
-                        print(date2);},
-                       selectionMode: DateRangePickerSelectionMode.range,
-                       maxDate: DateTime.now(),
-                       minDate: DateTime.parse(Provider.of<UserModel>(context, listen: false).creationDate),
+                        print(date2);
+                      },
+                      selectionMode: DateRangePickerSelectionMode.range,
+                      maxDate: DateTime.now(),
+                      minDate: DateTime.parse(
+                          Provider.of<UserModel>(context, listen: false)
+                              .creationDate),
                     ),
-                ),
-                ElevatedButton(
-                onPressed: (){Navigator.pushNamed(context, 'HistoryView');},
-                child: Text("Ver", style: TextStyle(fontSize: 20),)),
-              ],
-            ))
-          ],
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, 'HistoryView',
+                            arguments: Tuple2(date1, date2));
+                      },
+                      child: Text(
+                        "Ver",
+                        style: TextStyle(fontSize: 20),
+                      )),
+                ],
+              ))
+            ],
+          ),
         ),
-      ),
-    );
+      );
   }
 }
-
--- lib/HistoryView.dart
+++ lib/HistoryView.dart
@@ -1,10 +1,10 @@
-
 import 'package:a_bit_of_health/Today.dart';
 import 'package:a_bit_of_health/models/FoodModel.dart';
 import 'package:a_bit_of_health/providers/FoodProvider.dart';
 import 'package:a_bit_of_health/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tuple/tuple.dart';
 
 import 'Login.dart';
 import 'models/UserModel.dart';
@@ -12,75 +12,70 @@ import 'models/UserModel.dart';
 class HistoryView extends StatelessWidget {
   const HistoryView({Key key}) : super(key: key);
 
-@override
+  @override
   Widget build(BuildContext context) {
-     if(Provider.of<UserModel>(context).userID ==null)
+    Tuple2<String, String> range = ModalRoute.of(context).settings.arguments;
+    if (Provider.of<UserModel>(context).userID == null)
       return Login();
     else
-         return Scaffold(
-      appBar:getAppBar(context:context),
-      body: Container(
-        decoration: BoxDecoration(
+      return Scaffold(
+        appBar: getAppBar(context: context),
+        body: Container(
+          decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage('fondo_historial.jpg'),
-              fit: BoxFit.cover
-            ),
+                image: AssetImage('fondo_historial.jpg'), fit: BoxFit.cover),
           ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-
-          children: [
-            getDirectionsBar(context , 'History'),
-            Expanded(child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  'Registros desde 01-05-2021 Hasta 10-05-2021'
-                ),
-                Expanded(
-                                  child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              getDirectionsBar(context, 'History'),
+              Expanded(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('Registros desde ${range.item1} hasta ${range.item2}',
+                      style: TextStyle(fontFamily: 'Mont2', fontSize: 20)),
+                  Expanded(
+                      child: Container(
                     color: Colors.white.withOpacity(0.8),
-                    width: MediaQuery.of(context).size.width*0.5,
-                    height: MediaQuery.of(context).size.height/2.2,
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    height: MediaQuery.of(context).size.height / 2.2,
                     child: FutureBuilder(
-                      future: FoodProvider.getUserRegisterRange(
-                      Provider.of<UserModel>(context, listen:false).userID,
-                      '',
-                      ''),
-                      builder: (context ,snapshot ){
-                        if(snapshot.hasData){
-
-                        return  ListView.builder(
-                        itemBuilder: (context , i ){
-
-                          return getRegisterList(context, FoodRegister(), false);
+                        future: FoodProvider.getUserRegisterRange(
+                            Provider.of<UserModel>(context, listen: false)
+                                .userID,
+                            '',
+                            ''),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return ListView.builder(
+                              itemBuilder: (context, i) {
+                                return getRegisterList(
+                                    context, FoodRegister(), false);
+                              },
+                            );
+                          } else {
+                            return CircularProgressIndicator();
+                          }
+                        }),
+                  )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                        child: Text('Atras'),
+                        onPressed: () {
+                          Navigator.pop(context);
                         },
-
-                      ); }
-                      else{
-                        return CircularProgressIndicator();
-                      }
-                    
-                      }
-                  ),)
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ElevatedButton(
-                      child: Text('Atras'),
-                      onPressed: (){
-                        Navigator.pop(context);
-                      },
-                    ),
-                    ElevatedButton(onPressed: (){}, child: Text('Descargar'))
-                  ],
-                )
-              ],
-            ))
-          ],
+                      ),
+                      ElevatedButton(onPressed: () {}, child: Text('Descargar'))
+                    ],
+                  )
+                ],
+              ))
+            ],
+          ),
         ),
-      ),
-    );
+      );
   }
 }
